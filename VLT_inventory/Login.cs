3dd8e0f08c6036f0f975175f0581420d54980ce7
wshirey55 <wshirey@sciotodowns.com>
2015-02-27 00:08:49 -0500

    Working Connectionstring
    Finally got the connection string to work on the login page.

--- VLT_inventory/Login.cs
+++ VLT_inventory/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace VLT_inventory
 {
@@ -16,5 +17,23 @@ namespace VLT_inventory
         {
             InitializeComponent();
         }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            SqlConnection myConnection = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=vlt_inventoryDB;Integrated Security=True");
+           
+            try
+            { 
+                myConnection.Open();
+                 MessageBox.Show("Well Done!");
+            }
+
+                catch(SqlException ex)
+            {
+                    MessageBox.Show("You Failed"+ ex.Message);
+                }
+            }
+            
+        }
     }
-}
+
