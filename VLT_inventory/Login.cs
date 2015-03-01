35d2779943e55364d462338babeb926b8e55eaa5
wshirey55 <wshirey@sciotodowns.com>
2015-03-01 16:56:51 -0500

    Working Login and Connection String
    The login currently opens to the navigation page

--- VLT_inventory/Login.cs
+++ VLT_inventory/Login.cs
@@ -19,13 +19,42 @@ namespace VLT_inventory
         }
 
         private void btn_login_Click(object sender, EventArgs e)
-        {
+        {   //data string to open sql server connection
             SqlConnection myConnection = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=vlt_inventoryDB;Integrated Security=True");
            
             try
             { 
+                //open conection to read login information
                 myConnection.Open();
-                 MessageBox.Show("Well Done!");
+                SqlCommand cmd = new SqlCommand("SELECT * from dbo.Users where User_Name = '" + txt_login.Text +"' and Password = '" + txt_password.Text + "'", myConnection);
+                SqlDataReader dr;
+                dr = cmd.ExecuteReader();
+
+                int count = 0;
+                while (dr.Read())
+                    {
+                        count ++;
+                    }
+
+                if (count == 1)
+                     {
+                         this.Hide();
+
+                         Home f1 = new Home();
+                         f1.ShowDialog();
+                    
+                         this.Close();
+                     }
+                else if (count > 1)
+                {
+                    MessageBox.Show("Duplicate username and password. Please contact your system admin.");
+                }
+
+                else
+                {
+                    MessageBox.Show("Username and/or Password is incorrect.");
+                }
+
             }
 
                 catch(SqlException ex)
